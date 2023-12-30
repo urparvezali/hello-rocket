0d24d47897270d930c1b783cecb34fd096b5b17f
Parvez Ali <urparvezali@gmail.com>
2023-12-30 19:26:06 +0600

    more learning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use std::io;
+
+use rocket::tokio::{
+    task::spawn_blocking,
+    time::{sleep, Duration},
+};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[get("/parvez")]
+fn subindex() -> &'static str {
+    "This page created by the owner, Parvez Ali"
+}
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+#[get("/blocking_task")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    let vec = spawn_blocking(|| std::fs::read("data.txt"))
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+    Ok(vec)
+}
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index, subindex, delay, blocking_task])
+        .launch()
+        .await?;
+
+    Ok(())
 }
